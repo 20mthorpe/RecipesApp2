cf2f0aa8c90b67e7c4c6869a6c93efeabaa205e3
20mthorpe <tho20019@byui.edu>
2023-06-17 16:05:40 -0600

    Initial Commit

---
+++ RecipesApp2/RecipesApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp2App.swift
+//  RecipesApp2
+//
+//  Created by Mary Thorpe on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
